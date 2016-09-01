99999d51fc2674104ab0a37aa2f50bdec34539d3
runforever <c.chenchao.c@gmail.com>
2016-09-01 13:36:26 +0800

    update local config

--- lisp/init-local.el
+++ lisp/init-local.el
@@ -1,13 +1,20 @@
 ;;; set nu
 (global-linum-mode t)
 
+;;; flycheck
+(require 'flycheck)
+
+;;; tab width
+(setq-default indent-tabs-mode nil)
+(setq-default tab-width 4)
+(setq indent-line-function 'insert-tab)
+
 ;;; encoding setting
 (prefer-coding-system 'utf-8)
 (setq default-buffer-file-coding-system 'utf-8)
 
-;;; tab width
-(setq default-tab-width 4)
-(setq-default indent-tabs-mode nil)
+;;; font settings
+(set-frame-font "Inconsolata-19")
 
 ;;; not save temp file
 (setq auto-save-default nil)
@@ -16,7 +23,7 @@
 ;;; useful plugin
 ;;; smartparents
 (require 'smartparens-config)
-(smartparens-global-strict-mode +1)
+(smartparens-global-mode +1)
 
 ;;; window-numbering
 (window-numbering-mode t)
@@ -32,6 +39,7 @@
 (require 'org)
 (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))
 (global-font-lock-mode 1)
+(setq org-export-with-sub-superscripts nil)
 
 ;;; 加载markdown
 (eval-after-load "org" '(require 'ox-md nil t))
@@ -43,5 +51,97 @@
 (setq org-octopress-directory-org-top   "~/CodeRepo/GitRepo/octopress/source")
 (setq org-octopress-directory-org-posts "~/CodeRepo/GitRepo/octopress/source/blog")
 (setq org-octopress-setup-file          "~/CodeRepo/GitRepo/octopress/org-sty/setupfile.org")
+
+;;; go mode
+(require 'go-mode-load)
+
+;;; go format
+(add-hook 'go-mode-hook
+          (lambda ()
+            (setq tab-width 4)
+            (setq indent-tabs-mode nil)))
+
+;;; paredit in clojure mode
+(after-load 'clojure-mode
+  (add-hook 'clojure-mode-hook 'paredit-everywhere-mode))
+
+;;; python
+(require 'compile)
+(require 'python-pep8)
+(require 'python)
+
+(setq
+ python-shell-interpreter "ipython"
+ python-shell-interpreter-args "--pylab"
+ python-shell-prompt-regexp "In \\[[0-9]+\\]: "
+ python-shell-prompt-output-regexp "Out\\[[0-9]+\\]: "
+ python-shell-completion-setup-code
+ "from IPython.core.completerlib import module_completion"
+ python-shell-completion-module-string-code
+ "';'.join(module_completion('''%s'''))\n"
+ python-shell-completion-string-code
+ "';'.join(get_ipython().Completer.all_completions('''%s'''))\n")
+
+(add-hook 'after-init-hook #'global-flycheck-mode)
+
+(add-hook 'python-mode-hook #'(lambda () (setq flycheck-checker 'python-pylint)))
+
+;;; remove tailwhitespace
+(add-hook 'before-save-hook (lambda () (whitespace-cleanup)))
+
+;;; indent
+(setq indent-tabs-mode nil)
+(setq tab-width 4)
+(setq tab-stop-list ())
+
+;;; web auto indent
+(require 'web-mode)
+
+(add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.css\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+
+;(setq web-mode-markup-indent-offset 4)
+;(setq web-mode-css-indent-offset 4)
+;(setq web-mode-code-indent-offset 4)
+
+(add-hook 'web-mode-hook 'emmet-mode)
+(add-hook 'sgml-mode-hook 'emmet-mode)
+(add-hook 'css-mode-hook 'emmet-mode)
+
+(setq flycheck-disabled-checkers '(html-tidy))
+
+;;; ox-rsg
+(require 'ox-rst)
+
+;;; elixir
+(unless (package-installed-p 'elixir-mode)
+  (package-install 'elixir-mode))
+
+(add-to-list 'auto-mode-alist '("\\.elixir2\\'" . elixir-mode))
+
+(sp-with-modes '(elixir-mode)
+  (sp-local-pair "fn" "end"
+                 :when '(("SPC" "RET"))
+                 :actions '(insert navigate))
+  (sp-local-pair "do" "end"
+                 :when '(("SPC" "RET"))
+                 :post-handlers '(sp-ruby-def-post-handler)
+                          :actions '(insert navigate)))
+
+(unless (package-installed-p 'alchemist)
+  (package-install 'alchemist))
+
+(setq alchemist-mix-command "/usr/local/bin/mix")
+(setq alchemist-execute-command "/usr/local/bin/elixir")
+(setq alchemist-compile-command "/usr/local/bin/elixirc")
+
+;;; restful client
+(require 'restclient)
+
+;;; multi term
+(require 'multi-term)
+(setq multi-term-program "/bin/zsh")
+(setq system-uses-terminfo nil)
 
 (provide 'init-local)
